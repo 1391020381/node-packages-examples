105aa67befc67500a259c82694b25e152cc13865
1391020381 <1391020381@qq.com>
2022-04-22 11:34:03 +0800

    update

--- vue2-router-vuex-demo/src/router/myVueRouter.js
+++ vue2-router-vuex-demo/src/router/myVueRouter.js
@@ -1,6 +1,43 @@
 let Vue = null;
-class VueRouter {
 
+class HistoryRoute {
+  constructor() {
+    this.current = null
+  }
+}
+class VueRouter {
+  constructor(options) {
+    this.mode = options.mode || 'hash'
+    this.routes = options.routes || []
+    this.routesMap = this.createMap(this.routes)
+    this.history = new HistoryRoute()
+    this.init()
+  }
+  init() {
+    if (this.mode === 'hash') {
+      location.hash ? '' : location.hash = '/'
+      window.addEventListener('load', () => {
+        this.history.current = location.hash.slice(1)
+      })
+      window.addEventListener('hashchange', () => {
+        this.history.current = location.hash.slice(1)
+      })
+    } else {
+      location.pathname ? '' : location.pathname = '/';
+      window.addEventListener('load', () => {
+        this.history.current = location.pathname
+      })
+      window.addEventListener('popstate', () => {
+        this.history.current = location.pathname
+      })
+    }
+  }
+  createMap(routes) {
+    return routes.reduce((pre, current) => {
+      pre[current.path] = current.component
+      return pre;
+    }, {})
+  }
 }
 
 VueRouter.install = function (vue) {
@@ -18,16 +55,29 @@ VueRouter.install = function (vue) {
           return this._root._router
         }
       })
+      Object
+        .defineProperty(this, '$route', {
+          get() {
+            return this._root._router.history.current
+          }
+        })
     }
   })
   Vue.component('router-link', {
+    props: {
+      to: String
+    },
     render(h) {
-      return h('a', {}, '首页')
+      let mode = this._self._root._router.mode
+      let to = mode = 'hash' ? '#' + this.to : this.to
+      return h('a', { attrs: { href: to } }, this.$slots.default)
     }
   })
   Vue.component('router-view', {
     render(h) {
-      return h('h1', {}, '首页视图')
+      let current = this._self._root._router.history.current;
+      let routeMap = this._self._root._router.routesMap;
+      return h(routeMap[current])
     }
   })
 }
